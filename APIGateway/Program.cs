c29cc388e5200068460c4e4b5d1500823f695dc1
Salitha-Dilshan-IIT <w2121689@westminster.ac.uk>
2025-08-09 12:01:54 +0530

    add new domain

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
-                "https://resttrackweb-bqe3h2bta2erguet.canadacentral-01.azurewebsites.net"
+                "https://resttrackweb-bqe3h2bta2erguet.canadacentral-01.azurewebsites.net",
+                "https://restrack.online/"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
